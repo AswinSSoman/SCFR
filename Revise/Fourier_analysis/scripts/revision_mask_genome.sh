5539460c8841e72a8feac669d0947620744fa74e
Aswin S Soman <ceglab3@gmail.com>
2026-09-01 18:06:11 +0530

    code update 606

--- Revise/Fourier_analysis/scripts/revision_mask_genome.sh
+++ Revise/Fourier_analysis/scripts/revision_mask_genome.sh
@@ -88,7 +88,7 @@ scp pseudo_hits.bed genome.fully_masked.fa ceglab25@172.28.65.125:/media/aswin/S
 
 
 ######################################################################################################################################################################################################################################################################################################
-
+#Visualization
 
 cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300/Non_homology
 time makeblastdb -in genome.fully_masked.fa -out genome.fully_masked.fa -dbtype nucl
@@ -107,20 +107,23 @@ igvtools sort /home/ceglab8/Downloads/SCFR/GCF_009914755.1_T2T-CHM13v2.0_genomic
 #Generate the .idx index file
 igvtools index /home/ceglab8/Downloads/SCFR/GCF_009914755.1_T2T-CHM13v2.0_genomic.sorted.gtf
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Intergenic SCFRs
 
-#Check overlap between intergenic SCFR & regions with homology
-cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300
-bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique.bed -b Non_homology/pseudo_hits.bed | wc -l
-bedtools intersect -a human_scfr_all_atleast_300bp_antisense_intergenic.bed -b Non_homology/pseudo_hits.bed | wc -l
+	#Check overlap between intergenic SCFR & regions with homology
+	cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300
+	bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique.bed -b Non_homology/pseudo_hits.bed | wc -l
+	bedtools intersect -a human_scfr_all_atleast_300bp_antisense_intergenic.bed -b Non_homology/pseudo_hits.bed | wc -l
 
-#Save Intergenic SCFRs with no overlap with homolohy regions
-bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique.bed -b Non_homology/pseudo_hits.bed -v > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed
-bedtools intersect -a human_scfr_all_atleast_300bp_antisense_intergenic.bed -b Non_homology/pseudo_hits.bed -v > human_scfr_all_atleast_300bp_antisense_intergenic_with_no_homology.bed
+	#Save Intergenic SCFRs with no overlap with homolohy regions
+	bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique.bed -b Non_homology/pseudo_hits.bed -v > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed
+	bedtools intersect -a human_scfr_all_atleast_300bp_antisense_intergenic.bed -b Non_homology/pseudo_hits.bed -v > human_scfr_all_atleast_300bp_antisense_intergenic_with_no_homology.bed
 
-#scp human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed human_scfr_all_atleast_300bp_antisense_intergenic_with_no_homology.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+	#scp human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed human_scfr_all_atleast_300bp_antisense_intergenic_with_no_homology.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
 
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Concatenate DFT results with pure intergenic SCFRs with no homology
+
 time while read s
 do
 sid=$(echo -e "$s" | awk -F "\t" '{print$1,$2,$3,"frame"$4}' OFS="_" | tr "-" "_")
@@ -129,19 +132,12 @@ echo $s $dtf
 unset sid dtf
 done < human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed	| sed 's/[ ]\+/\t/g' > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_dft_results.bed
 
-
-time LC_ALL=C awk -F'\t' -v OFS='\t' '
-# --- Pass 1: load summary.tsv into memory, keyed by its first column ---
-NR==FNR {
+time LC_ALL=C awk -F'\t' -v OFS='\t' 'NR==FNR {
     data[$1] = $0
     next
-}
-
-# --- Pass 2: stream the bed file, build matching key, look up in hash ---
-{
+}{
     chrom = $1
     gsub(/\./, "_", chrom)          # NC_060925.1 -> NC_060925_1
-
     frame = $4
     if (frame ~ /^-/) {
         gsub(/-/, "_", frame)        # -2 -> _2
@@ -149,7 +145,6 @@ NR==FNR {
     } else {
         key = "SCFR_" chrom "_" $2 "_" $3 "_frame" frame   # 1 -> frame1
     }
-
     lookup = key "_fft"
     out = $0
     if (lookup in data) {
@@ -160,11 +155,154 @@ NR==FNR {
         out = out "\t" rest
     }
     print out
-}
-' /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300/human_scfr_all_atleast_300bp_only_intergenic_unique/chromosome_wise_summary/summary.tsv \
-  human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed \
-  > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_dft_results.bed
+}' /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300/human_scfr_all_atleast_300bp_only_intergenic_unique/chromosome_wise_summary/summary.tsv human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology.bed > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_dft_results.bed
 
 
+#View in IGV
+
+/home/ceglab8/workspace/1.master_thesis/6.source_codes/IGV_Linux_2.5.0/igv.sh
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#To view onlne
+
+#Filter only SCFRs with 0.3 DFT frequency
+awk -F "\t" '$10~/0\.3/' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_dft_results.bed > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed
+awk -F "\t" '$10~/0\.33/' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_dft_results.bed > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed
+
+#Visualize length
+awk '{print$3-$2}' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed | statplot.R --biplot human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results_length_distribution.png
+awk '{print$3-$2}' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed | statplot.R --biplot human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results_length_distribution.png
+awk -F "\t" '{print$3-$2}' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed | sort | uniq -c | sed 's/^[ ]\+//g' | tr " " "\t" | ./term_hist2.py -s 50 -H 30 | less -SR
+
+#sort based on length
+awk -F "\t" 'BEGIN{OFS="\t"} {print ($3 - $2), $0}' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed | sort -k1,1nr | cut -f2- > length_sorted_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed
+
+#Add header
+sed -i '1i Chr Start End Frame_strand Filler Strand SCFR_Name Num_Raw_Peaks Top_Peak_Count Frequencies Magnitudes Periods' length_sorted_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed 
+#make tab separated
+sed -i 's/[ ]\+/\t/g' length_sorted_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed
+
+#Make it standard bed format: Take 1st 6 columns
+awk -F "\t" '{print$1,$2,$3,$4,$5,$6}' OFS="\t" human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed > ucsc.bed
+
+#scp human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed ucsc.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+#scp human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Download NCBI Refseq Functional elements for human T2T genome from UCSC
+
+wget https://ftp.ncbi.nlm.nih.gov/refseq/FunctionalElements/trackhub/data/human/GCF_009914755.1-RS_2023_10/FEbiolregions_RS_2023_10_T2T-CHM13v2.0.bb
+bigBedToBed FEbiolregions_RS_2023_10_T2T-CHM13v2.0.bb FEbiolregions_RS_2023_10_T2T-CHM13v2.0.bed
+
+#Rename chromosome IDs
+awk -F "\t" 'FNR==NR {map[$1]=$2; next} $1 in map {$1=map[$1]} 1' OFS="\t" /media/aswin/SCFR/SCFR-main/genomes/human/map.tsv FEbiolregions_RS_2023_10_T2T-CHM13v2.0.bed > tempFE.bed
+sed -i 's/\r//' tempFE.bed
+awk -F'\t' 'BEGIN{OFS="\t"} {
+    # Combine everything from column 10 onwards into a single block of text
+    desc = $10
+    for(i=11; i<=NF; i++) {
+        if($i != "") desc = desc "; " $i
+    }
+    # Remove hidden Windows carriage returns and internal line breaks from the text
+    gsub(/\r/, "", desc)
+    gsub(/\n/, " ", desc)
+    
+    # Print a standard 12-column BED format
+    print $1,$2,$3,$4,$5,$6,$7,$8,$9,1,($3-$2),0,desc
+}' tempFE.bed > NCBI_RefSeq_Functional_Elements_GCF_009914755.1-RS_2023_10_biological_regions.bed 
+
+#scp NCBI_RefSeq_Functional_Elements_GCF_009914755.1-RS_2023_10_biological_regions.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+#scp NCBI_RefSeq_Functional_Elements_GCF_009914755.1-RS_2023_10_biological_regions_fomratted.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Download RepeatMasker data for human from UCSC
+
+cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300
+wget https://hgdownload.soe.ucsc.edu/gbdb/hs1/t2tRepeatMasker/chm13v2.0_rmsk.bb
+bigBedToBed chm13v2.0_rmsk.bb chm13v2.0_rmsk.bed
+
+awk -F'"' '{
+    refseq = ""; ucsc = "";
+    for(i=1; i<=NF; i++) {
+        if($i == "refseqAccession") refseq = $(i+2)
+        if($i == "ucscStyleName") ucsc = $(i+2)
+    }
+    if(refseq != "" && ucsc != "") print  ucsc "\t" refseq
+}' /media/aswin/SCFR/SCFR-main/genomes/human/sequence_report.jsonl > refseq_to_chrnames.tsv
+
+awk -F "\t" 'FNR==NR {map[$1]=$2; next} $1 in map {$1=map[$1]} 1' OFS="\t" refseq_to_chrnames.tsv chm13v2.0_rmsk.bed > Repetitive_Elements.bed
+
+#scp Repetitive_Elements.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Some borderline cases occurs i.e. some SCFRs sometimes overlaps with both intergenic & genic features. But since the overlap length with intergenic reigion is >300bp these would be saved inside "human_scfr_all_atleast_300bp_only_intergenic_unique.bed"
+#Partially these SCFRs overlap with genic regions, importantly CDS, which contributes to it's 0.33 DFT frequency, hence these can't be proto-genes & must be removed
+#Hence identify all such SCFRs (in the intergenic region which has no homology with human CDS & has DFT frequency 0.33) that has overlap with genic regions
+
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/genes.bed -wo | awk '{print$NF}' | ministat 
+
+cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300
+mkdir /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300/annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results
+
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/genes.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_genes.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/cds_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_cds_unique.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/pseudogene_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_pseudogene_unique.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/RNA_genes_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_RNA_genes_unique.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/utr_refined_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_utr_refined_unique.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/only_introns_refined_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_only_introns_refined_unique.bed
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/only_intergenic_unique.bed -wo > annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results/overlapping_only_intergenic_unique.bed
+
+#check overlaps between bed files
+cd /media/aswin/SCFR/SCFR-main/Fourier_analysis/human/300/annotation_overlaping_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results
+./bed_overlap_terminal.py overlapping_cds_unique.bed overlapping_genes.bed overlapping_only_intergenic_unique.bed overlapping_only_introns_refined_unique.bed overlapping_pseudogene_unique.bed overlapping_RNA_genes_unique.bed overlapping_utr_refined_unique.bed
+./bed_overlap_matrix.py overlapping_cds_unique.bed overlapping_genes.bed overlapping_only_intergenic_unique.bed overlapping_only_introns_refined_unique.bed overlapping_pseudogene_unique.bed overlapping_RNA_genes_unique.bed overlapping_utr_refined_unique.bed
+
+#NOTE: SCFRs overlapping genes are overlapping with all SCFRs overlapping other features like UTR, CDS, introns, RNA-genes, etc
+
+bedtools intersect -a human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results.bed -b final_gtf_features/genes.bed -v > human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results_no_overlap_with_gene.bed
+awk -F "\t" 'BEGIN{OFS="\t"} {print ($3 - $2), $0}' human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results_no_overlap_with_gene.bed | sort -k1,1nr | cut -f2- > length_sorted_human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.3_dft_results.bed
+
+#scp human_scfr_all_atleast_300bp_only_intergenic_unique_with_no_homology_with_0.33_dft_results_no_overlap_with_gene.bed ceglab8@172.28.65.118:~/Downloads/SCFR/
+
+
+
+
+NC_060935.1:71,303,515-71,457,578
+
+chr11:71,314,104-71,332,604
+NC_060935.1:71,314,104-71,332,604
+chr2:5,038,714-5,048,238
+NC_060926.1:5,038,714-5,048,238
+
+
+
+
+#Observations:
+
+Region: NC_060935.1:206502-214065 
+#Length:7563
+#This region in human T2T genome harbors a longest intergenic SCFR with no homology to any human CDS & has 0.33 DFT frequency & this region is absent in other assembly, hence this can be used as a reason to use T2T assemblies
+#These tracks show the regions unique to the T2T-CHM13 v2.0 assembly compared to the GRCh38/hg38 and GRCh37/hg19 reference assemblies (Position: chr11:206151-220824)
+#This regions contains a SEDEF Segmental Duplications in the exact regions SCFRs covering
+#Simple tandem repeats are found here, no other repeats
+
+Region: NC_060942.1:78744852-78749265
+
+#This region is only shared with chimpanzee but absent in gorilla & oranguttan
+#SEDEF Segmental Duplications found here
+#Simple tandem repeats are found here, no other repeats
+#LINE element aurrounding this SCFR, could lead to activation of transcription
+
+Region: NC_060926.1:5042518-5045731 
+#Length: 3213
+#This region is only unique to human absent in all other primates
+#Contain only simple repeats : (CTACTA)n
+
+
+
+
+
+SCFR_NC_060935_1_206502_214065_frame1_fft
 
 
